69e2f69cd6959fbad2634628da2e7796ddee0235
Bharat Arimilli <bbarimilli@outlook.com>
2017-08-31 23:19:21 -0500

    Add attachment support

--- index.js
+++ index.js
@@ -3,59 +3,66 @@ const client = require('sendgrid')(SENDGRID_KEY);
 const helper = require('sendgrid').mail;
 const async = require('async');
 
-function sendEmail(to, subject, body, template) {
-    return new Promise((resolve, reject) => {
-      const fromEmail = new helper.Email('web@utdmercury.com');
-      const toEmail = new helper.Email(to);
-      const content = new helper.Content('text/html', body);
-      const mail = new helper.Mail();
+function sendEmail(to, subject, body, template, attachments) {
+  return new Promise((resolve, reject) => {
+    const fromEmail = new helper.Email('web@utdmercury.com');
+    const toEmail = new helper.Email(to);
+    const content = new helper.Content('text/html', body);
+    const mail = new helper.Mail();
 
-      mail.setSubject(subject);
-      mail.addContent(content);
-      mail.setFrom(fromEmail);
-      mail.setTemplateId(template);
+    mail.setSubject(subject);
+    mail.addContent(content);
+    mail.setFrom(fromEmail);
+    mail.setTemplateId(template);
 
-      const personalization = new helper.Personalization();
-      personalization.addTo(toEmail);
-      mail.addPersonalization(personalization);
+    const personalization = new helper.Personalization();
+    personalization.addTo(toEmail);
+    mail.addPersonalization(personalization);
 
-      const request = client.emptyRequest({
-          method: 'POST',
-          path: '/v3/mail/send',
-          body: mail.toJSON()
-      });
+    if (attachments) {
+      for (let i = 0; i < attachments.length; i++) {
+        const attachment = new helpers.Attachment(attachments[i]);
 
-      client.API(request, (error, response) => {
-        if (error) {
-            reject(error)
-        } else {
-            resolve(response);
-        }
-      });
+        mail.addAttachment(attachment);
+      }
+    }
+
+    const request = client.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
     });
+
+    client.API(request, (error, response) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(response);
+      }
+    });
+  });
 }
 
 module.exports = function (context, message) {
-    if (message) {
-        if (message.environment === "production") {
-            async.each(message.users, (user, callback) => {
-                sendEmail(user.email, message.subject, message.body, message.template)
-                    .then(() => {
-                        callback();
-                    })
-                    .catch((e) => {
-                        callback();
-                    });
-            }, (err) => {
-                context.log('JavaScript queue trigger function processed work item');
-                
-                context.done();
-            });
-        } else {
-            context.log('Job received');
-        }
-        
-    } else {
+  if (message) {
+    if (message.environment === "production") {
+      async.each(message.users, (user, callback) => {
+        sendEmail(user.email, message.subject, message.body, message.template, message.attachments)
+          .then(() => {
+            callback();
+          })
+          .catch((e) => {
+            callback();
+          });
+      }, (err) => {
+        context.log('JavaScript queue trigger function processed work item');
+          
         context.done();
+      });
+    } else {
+      context.log('Job received');
     }
+  } else {
+    context.done();
+  }
 };
